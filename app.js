73482fd257716b3491030ccb08c882e8a2ccf4d4
YJChan <yjtoro@hotmail.com>
2020-03-04 17:54:20 +0800

    add new api endpoint

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var app = express();
-var ratesRouter = require('./routes/rates');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const app = express();
+const ratesRouter = require('./routes/rates');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/rates.js
+++ routes/rates.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const fetchUrl = require('fetch').fetchUrl;
+const apiKey = '';
 
 router.get('/exchgrate', function(req, res, next) {
-var endpoint = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+  const endpoint = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 
   fetchUrl(endpoint, { method: "get" }, function (error, meta, body) {    
-    var doc = body.toString();
+    const doc = body.toString();
     res.set('Content-Type', 'text/xml');
     res.send(doc);
+  });
+});
+
+router.get('/daily-rates', function(req, res, next) {
+  const endpoint = `http://data.fixer.io/api/latest?access_key=${apiKey}&format=1`;
+
+  fetchUrl(endpoint, {method: "get"}, function(err, meta, body) {
+    const data = body.toString();
+    res.set('Content-Type', 'application/json');
+    res.end(data);
   });
 });
 
